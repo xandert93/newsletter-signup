32ac6ad980072f05be88f728749e071298f20bfb
Xander T <xandert.93@outlook.com>
2020-12-04 14:46:35 +0000

    edited port again

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ app.post('/', (req, res) => {
 
 app.post('/failure', (req, res) => res.redirect('/'));
 
-const port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+let myPort = process.env.PORT || 3000;
+app.listen(myPort);
